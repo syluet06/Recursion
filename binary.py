9456303d5f3a0fe21fce00aa037dbde4c53a831a
Soner Say <soner.say0670@gmail.com>
2017-07-13 20:40:20 +0300

    Update binary.py

--- binary.py
+++ binary.py
@@ -2,8 +2,8 @@ def binary(s):
     if s is 0:
         return ""
     else:
-        return str(s%2)+binary(int(s/2))
+        return binary(int(s/2))+str(s%2)
 
 
 a = input()
-print("%s:%s" % (a,binary(a)[::-1]))
+print("%s:%s" % (a,binary(a)))
